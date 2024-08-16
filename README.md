2a83fc82c38187e3bb5e3580cd389514bac52e2f
fweinelt <fabian9119@gmx.net>
2024-08-16 14:53:08 +0200

    Release v0.1.0

--- README.md
+++ README.md
@@ -1,3 +1,130 @@
-# pytok
+# PyTok - a Python implementation of the TikTok API
 
-Working on a TikTok API implementation into Python for easy use. Stay tuned.
+![License](https://img.shields.io/badge/license-MIT-blue.svg)
+![Version](https://img.shields.io/badge/version-0.1.0-brightgreen.svg)
+
+## Table of Contents
+- [Introduction](#introduction)
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Configuration](#configuration)
+- [TODOs](#todos)
+- [Changelog](#changelog)
+
+## Introduction
+
+A Python implementation of the TikTok API enables interaction with TikTok's services through programmatic access. It typically involves OAuth2 authentication to securely complete the login process and subsequently access the users data. You can retrieve user profiles, including basic information like usernames and IDs or automate more advanced tasks like posting and managing content, such as videos and comments. Rate limiting and API quotas should be respected to avoid service interruptions. API keys from TikTok's [developer portal](https://developers.tiktok.com/doc/overview/) are an essential prerequisit for integration, though a sandbox is sufficient for most test cases.
+
+If you have suggestions, feedback or are looking to collaborate be sure to contact me at [fabian.weinelt@uni-bayreuth.de](mailto:fabian.weinelt@uni-bayreuth.de).
+
+## Features
+
+Currently implemented API features include
+
+### LoginKit
+
+The LoginKit class provides a comprehensive solution for interacting with TikTok's OAuth 2.0 authentication system. It is designed to facilitate the OAuth authentication workflow with TikTok, handle web server requests for OAuth and callbacks, and manage the resulting access tokens for use in making API requests. Features include
+
+- **Login using a browser:** Using `oauth_server` one can initiate a login workflow, which prompts the user in a browser window to log in via username and password and grant user specified permissions (scopes).This is done by starting a local TCP server to handle the authentification as well as the callback. Upon success, one recieves the access token for the requested scopes that can be used in conjuction with other API constituents. 
+
+- **Login using a QR-Code**: Alternatively one may use a QR-Code to login from the mobile app instead of a username and password. By executing `generate_qr_code` one recieves a QR url used to generate the corresponding QR-Code. By initiating `oath_qrcode` PyTok waits for the code to be scanned and, upon success, recieves the access token exactly like the browser login method.
+
+- **Multiple instances:** With now use of class methods, PyTok allows for as many instances as you want. Programatically controlling multiple TikTok accounts or using multiple apps is therefore possible.
+
+- **Access Token Management:** Methods like `refresh_access_token`, and `revoke_access_token` manage the lifecycle of access tokens, refreshing and revoking tokens as needed. The current access token can always be accessed by its property attribute `current_access_token`.
+
+- **Initialization:** The `__init__` method initializes essential parameters including client credentials, server port, and OAuth settings. It also generates necessary values for CSRF state and code verifier used in the authentification process.
+
+- **Properties:** Several properties (BASE_API_URL, CSRF_STATE_LENGTH, CODE_VERIFIER_LENGTH, etc.) have been defined to provide read-only access to important configuration values, ensuring encapsulation and controlled access to internal state.
+
+## Installation
+
+PyTok will soon be availabe via the Python Package Index (PyPI) und thus easily installable via ´pip´. Stay tuned...
+
+## Usage
+
+Here a few examples are brought up. Use them for testing and modify them to your needs.
+
+# LoginKit
+
+The simplest login procedure is done using ones browser. Firstly, an instance of `LoginKit` taking ones app client key and secret as mandatory arguments. By subsequently executing `oauth_server` one is prompted to sign into a TikTok account with their username and password. Upon success, the browser window displays "Authentification workflow completed" and can be closed. Lastly, the access token is printed to the terminal.
+
+```python
+from LoginKit import LoginKit
+    
+login_kit = LoginKit('YOUR_CLIENT_KEY', 'YOUR_CLIENT_SECRET')
+login_kit.oath_server()
+
+token = login_kit.current_access_token
+
+print(token)
+```
+
+The QR-Code workflow is a little bit more complicated. Again, an app client key and secret must be given. The QR URL as well as a token and client ticket are generated with it. Those are needed for security, so that no other program or user can interfer with the ongoing workflow. Pythons built-in `qrcode` library is then used to generate the code out of the URL. Furthermore `os` and `webbrowser` are used to save the newly created QR-Code image locally and display it in the webbrowser. On a mobile app go to "Profile", tap the burger menu at the top right and tap "My QR-Code". Tap the icon at the top right again to open the scanner and scan the QR-Code. Upon success, the access token will again be printed to the terminal.
+
+```python
+from LoginKit import LoginKit
+import qrcode
+import os
+import webbrowser
+    
+login_kit = LoginKit('YOUR_CLIENT_KEY', 'YOUR_CLIENT_SECRET')
+
+qr_url, qr_token, qr_client_ticket = login_kit.generate_qr_code()
+    
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(qr_url)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+    
+img_path = "qr_code.png"
+img.save(img_path)
+webbrowser.open_new_tab(f"file://{os.path.realpath(img_path)}")
+    
+login_kit.oauth_qrcode(qr_token, qr_client_ticket)
+
+print(login_kit.current_access_token)
+```
+
+## Configuration
+
+Initializing an instance of `LoginKit` may take several optional arguments. These are
+
+- `port` (int): Port used by the TCP server when calling `oauth_server`. It is configured to allow for reusage. Default value is 3456.
+- `scopes` (list of strings): List of permissions (scopes) one needs access to. Default value is "user.info.basic".
+- `redirect_uri` (string): Web redirect URI given in the app in the TikTok developers portal. Default value is "http://127.0.0.1:3456/callback/"
+- `authorization_url` (string): URL for authorization. Default value is "https://www.tiktok.com/v2/auth/authorize/"
+- `base_api_url` (string): Base URL for most of TikToks API calls. Defualt value is "https://open.tiktokapis.com/v2/" 
+- `csrf_state_length` (int): Length of the anti-forgery token used in the server workflow. Default value is 16
+- `code_verifier_length` (int): Length of the generated code verifier. Must be between 43 and 128. Default value is 64.
+- `client_ticket_length` (int): Length of the generated client ticket used is the QR-Code workflow. URL-encoded version must be under 512 characters. Default value is 64.
+- `request_header` (dict): Header for HTTP requests. Default value is "{'Content-Type': 'application/x-www-form-urlencoded'}"
+
+## TODOs
+
+A list of ideas and features you can expect in the future.
+- Implement ContentPostingAPI (WIP)
+- Implement proper error handling
+- Allow for updating the scopes during runtime
+- Make a PyPI release
+- Expand on the documentation
+
+## Changelog
+
+### 0.1.0
+
+- Introduced the proper README.md you are reading right now.
+- Specified data types for input arguments in LoginKit's __init__ method.
+- Updated LoginKit's attribute names.
+- LoginKit's `__SCOPES` attribute is now a set, was string before.
+
+### pre 0.1.0
+
+Experimented with different implementations, finally settling on a instance-based approach. Implemented the QR-Code workflow along the way.
--- src/LoginKit.py
+++ src/LoginKit.py
@@ -6,6 +6,7 @@ import random
 import hashlib
 import webbrowser
 import time
+from typing import Dict, List, Optional
 
 class LoginKit:
     class TCPServer(socketserver.TCPServer):
@@ -25,86 +26,93 @@ class LoginKit:
                 super().do_GET()
     
     def __init__(self,
-            client_key,
-            client_secret,
-            port=3456,
-            scopes='user.info.basic',
-            redirect_uri='http://127.0.0.1:3456/callback/',
-            authorization_url = 'https://www.tiktok.com/v2/auth/authorize/',
-            base_api_url='https://open.tiktokapis.com/v2/',
-            csrf_state_length = 16,
-            code_verifier_length = 64,
-            client_ticket_length = 64
+            client_key: str,
+            client_secret: str,
+            port: Optional[int] = 3456,
+            scopes: Optional[List[str]] = ['user.info.basic'],
+            redirect_uri: Optional[str] = 'http://127.0.0.1:3456/callback/',
+            authorization_url: Optional[str] = 'https://www.tiktok.com/v2/auth/authorize/',
+            base_api_url: Optional[str] = 'https://open.tiktokapis.com/v2/',
+            csrf_state_length: Optional[int] = 16,
+            code_verifier_length: Optional[int] = 64,
+            client_ticket_length: Optional[int] = 64,
+            request_header: Optional[Dict] = {'Content-Type': 'application/x-www-form-urlencoded'}
         ):
-        self._AUTHORIZATION_URL = authorization_url
-        self._BASE_API_URL = base_api_url
-        self._CSRF_STATE_LENGTH = csrf_state_length
-        self._CODE_VERIFIER_LENGTH = code_verifier_length
-        self._CLIENT_TICKET_LENGTH = client_ticket_length
-        self._client_key = client_key
-        self._client_secret = client_secret
-        self._port = port
-        self._redirect_uri = redirect_uri
-        self._scopes = scopes
-        self._oauth_success = False
-        self._csrf_state = ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~') for x in range(self._CSRF_STATE_LENGTH))
-        self._code_verifier = ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~') for x in range(self._CODE_VERIFIER_LENGTH))
-        self._current_access_token = None
-        self._current_refresh_token = None
+        self.__AUTHORIZATION_URL = authorization_url
+        self.__BASE_API_URL = base_api_url
+        self.__CSRF_STATE_LENGTH = csrf_state_length
+        self.__CODE_VERIFIER_LENGTH = code_verifier_length
+        self.__CLIENT_TICKET_LENGTH = client_ticket_length
+        self.__CLIENT_KEY = client_key
+        self.__CLIENT_SECRET = client_secret
+        self.__PORT = port
+        self.__REDIRECT_URI = redirect_uri
+        scopes.append('user.info.basic')
+        self.__SCOPES = set(scopes)
+        self.__OAUTH_SUCCESS = False
+        self.__CSRF_STATE = ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~') for x in range(self.__CSRF_STATE_LENGTH))
+        self.__CODE_VERIFIER = ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~') for x in range(self.__CODE_VERIFIER_LENGTH))
+        self.__CURRENT_ACCESS_TOKEN = None
+        self.__CURRENT_REFRESH_TOKEN = None
+        self.__REQUEST_HEADER = request_header
 
     # Property definitions
 
     @property
     def BASE_API_URL(self):
-        return self._BASE_API_URL
+        return self.__BASE_API_URL
 
     @property
     def CSRF_STATE_LENGTH(self):
-        return self._CSRF_STATE_LENGTH
+        return self.__CSRF_STATE_LENGTH
     
     @property
     def CODE_VERIFIER_LENGTH(self):
-        return self._CODE_VERIFIER_LENGTH
+        return self.__CODE_VERIFIER_LENGTH
     
     @property
     def CLIENT_TICKET_LENGTH(self):
-        return self._CLIENT_TICKET_LENGTH
+        return self.__CLIENT_TICKET_LENGTH
     
     @property
+    def scopes(self):
+        return self.__SCOPES
+
+    @property
     def oauth_success(self):
-        return self._oauth_success
+        return self.__OAUTH_SUCCESS
     
     @property
     def current_access_token(self):
-        return self._current_access_token
+        return self.__CURRENT_ACCESS_TOKEN
     
     @property
     def current_refresh_token(self):
-        return self._current_refresh_token
+        return self.__CURRENT_REFRESH_TOKEN
     
     @property
     def client_key(self):
-        return self._client_key
+        return self.__CLIENT_KEY
     
     @property
     def client_secret(self):
-        return self._client_secret
+        return self.__CLIENT_SECRET
 
     # Server OAuth handling
 
     def _handle_oauth(self, handler):
-        code_challenge = self._sha256_hash(self._code_verifier)
+        code_challenge = self._sha256_hash(self.__CODE_VERIFIER)
         params = {
-            'client_key': self._client_key,
-            'scope': self._scopes,
-            'redirect_uri': self._redirect_uri,
-            'state': self._csrf_state,
+            'client_key': self.__CLIENT_KEY,
+            'scope': ",".join(self.__SCOPES),
+            'redirect_uri': self.__REDIRECT_URI,
+            'state': self.__CSRF_STATE,
             'response_type': 'code',
             'code_challenge': code_challenge,
             'code_challenge_method': 'S256'
         }
         handler.send_response(302)
-        handler.send_header('Location', self._AUTHORIZATION_URL + '?' + urlparse.urlencode(params))
+        handler.send_header('Location', self.__AUTHORIZATION_URL + '?' + urlparse.urlencode(params))
         handler.end_headers()
 
     def _handle_callback(self, handler):
@@ -120,7 +128,7 @@ class LoginKit:
             handler.wfile.write(f"Error: {params.get('error_description', ['Unknown error'])[0]}".encode())
             return
 
-        if state != self._csrf_state:
+        if state != self.__CSRF_STATE:
             handler.send_response(400)
             handler.end_headers()
             handler.wfile.write("Error: Invalid state parameter".encode())
@@ -136,22 +144,22 @@ class LoginKit:
             handler.wfile.write(f"Error fetching access token: {token_response_data}".encode())
         else:
             handler.wfile.write(f"Authorization workflow successful".encode())
-            self._oauth_success = True
-            self._current_access_token = token_response_data.get('access_token')
-            self._current_refresh_token = token_response_data.get('refresh_token')
+            self.__OAUTH_SUCCESS = True
+            self.__CURRENT_ACCESS_TOKEN = token_response_data.get('access_token')
+            self.__CURRENT_REFRESH_TOKEN = token_response_data.get('refresh_token')
 
     def oath_server(self, timeout = 30, open_in_browser='new_tab'):
         handler = lambda *args, **kwargs: self.Handler(*args, login_kit=self, **kwargs)
-        with self.TCPServer(("", self._port), handler) as httpd:
-            print(f"Serving at port {self._port}")
+        with self.TCPServer(("", self.__PORT), handler) as httpd:
+            print(f"Serving at port {self.__PORT}")
             if open_in_browser == 'new_tab':
-                webbrowser.open_new_tab(f'http://127.0.0.1:{self._port}/oauth/')
+                webbrowser.open_new_tab(f'http://127.0.0.1:{self.__PORT}/oauth/')
             elif open_in_browser == 'new_window':
-                webbrowser.open_new(f'http://127.0.0.1:{self._port}/oauth/')
+                webbrowser.open_new(f'http://127.0.0.1:{self.__PORT}/oauth/')
             
             start_time = time.time()
             try:
-                while not self._oauth_success:
+                while not self.__OAUTH_SUCCESS:
                     httpd.handle_request()
                     if time.time() - start_time > timeout:
                         print('OAuth workflow timeout of '+str(timeout)+' seconds reached')
@@ -165,27 +173,26 @@ class LoginKit:
     
     def generate_qr_code(self):
         data = {
-            'client_key': self._client_key,
-            'scope': self._scopes,
-            'state': self._csrf_state
+            'client_key': self.__CLIENT_KEY,
+            'scope': ",".join(self.__SCOPES),
+            'state': self.__CSRF_STATE
         }
-        response = requests.post(self._BASE_API_URL+'oauth/get_qrcode/', headers={'Content-Type': 'application/x-www-form-urlencoded'}, data=data)
+        response = requests.post(self.__BASE_API_URL+'oauth/get_qrcode/', headers=self.__REQUEST_HEADER, data=data)
         response_data = response.json()
-
         if 'error' in response_data:
             return
         else:
-            client_ticket = urlparse.quote(''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~') for x in range(self._CLIENT_TICKET_LENGTH)))
+            client_ticket = urlparse.quote(''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~') for x in range(self.__CLIENT_TICKET_LENGTH)))
             scan_qrcode_url = response_data.get('scan_qrcode_url')
             return (scan_qrcode_url.replace('tobefilled', client_ticket), response_data.get('token'), client_ticket)
 
     def _check_qr_code_status(self, token):
         data = {
-            'client_key': self._client_key,
-            'client_secret': self._client_secret,
+            'client_key': self.__CLIENT_KEY,
+            'client_secret': self.__CLIENT_SECRET,
             'token': token
         }
-        response = requests.post(self._BASE_API_URL+'oauth/check_qrcode/', headers={'Content-Type': 'application/x-www-form-urlencoded'}, data=data)
+        response = requests.post(self.__BASE_API_URL+'oauth/check_qrcode/', headers=self.__REQUEST_HEADER, data=data)
         return response.json()        
 
     def oauth_qrcode(self, token, client_ticket, timeout=30, check_status_delay=1):      
@@ -203,9 +210,9 @@ class LoginKit:
                 if 'error' in token_response_data:
                     return
                 else:
-                    self._oauth_success = True
-                    self._current_access_token = token_response_data['access_token']
-                    self._current_refresh_token = token_response_data['refresh_token']
+                    self.__OAUTH_SUCCESS = True
+                    self.__CURRENT_ACCESS_TOKEN = token_response_data['access_token']
+                    self.__CURRENT_REFRESH_TOKEN = token_response_data['refresh_token']
                 return
 
             elif status == 'expired':
@@ -220,35 +227,33 @@ class LoginKit:
 
     def _fetch_access_token(self, code):
         data = {
-            'client_key': self._client_key,
-            'client_secret': self._client_secret,
+            'client_key': self.__CLIENT_KEY,
+            'client_secret': self.__CLIENT_SECRET,
             'code': code,
             'grant_type': 'authorization_code',
-            'redirect_uri': self._redirect_uri,
-            'code_verifier': self._code_verifier
+            'redirect_uri': self.__REDIRECT_URI,
+            'code_verifier': self.__CODE_VERIFIER
         }
-        response = requests.post(self._BASE_API_URL+'oauth/token/', headers={'Content-Type': 'application/x-www-form-urlencoded'}, data=data)
+        response = requests.post(self.__BASE_API_URL+'oauth/token/', headers=self.__REQUEST_HEADER, data=data)
         return response.json()
 
     def refresh_access_token(self):
         data = {
-            'client_key': self._client_key,
-            'client_secret': self._client_secret,
+            'client_key': self.__CLIENT_KEY,
+            'client_secret': self.__CLIENT_SECRET,
             'grant_type': 'refresh_token',
-            'refresh_token': self._current_refresh_token
+            'refresh_token': self.__CURRENT_REFRESH_TOKEN
         }
-        response = requests.post(self._BASE_API_URL+'oauth/token/', headers={'Content-Type': 'application/x-www-form-urlencoded'}, data=data)
+        response = requests.post(self.__BASE_API_URL+'oauth/token/', headers=self.__REQUEST_HEADER, data=data)
         return response.json()
 
     def revoke_access_token(self):
         data = {
-            'client_key': self._client_key,
-            'client_secret': self._client_secret,
-            'token': self._current_access_token
+            'client_key': self.__CLIENT_KEY,
+            'client_secret': self.__CLIENT_SECRET,
+            'token': self.__CURRENT_ACCESS_TOKEN
         }
-        response = requests.post(self._BASE_API_URL+'/oauth/revoke/', headers={
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }, data=data)
+        response = requests.post(self.__BASE_API_URL+'/oauth/revoke/', headers=self.__REQUEST_HEADER, data=data)
         return response.json()
 
     # Misc
@@ -259,10 +264,10 @@ class LoginKit:
         sha256.update(input_string.encode())
         return sha256.hexdigest()
     
-    def set_scopes(self, scopes):
+    def set__SCOPES(self, scopes):
         sc = scopes.replace(" ", "")
         scopelist = sc.split(',')
         for s in scopelist:
             if s not in ['user.info.basic', 'video.publish', 'video.upload', 'artist.certification.read', 'artist.certification.update', 'user.info.profile', 'user.info.stats', 'video.list']:
                 raise RuntimeWarning('Requested scope '+str(s)+' unknown, continuing...')
-        self._scopes = sc
+        self.__SCOPES = sc
